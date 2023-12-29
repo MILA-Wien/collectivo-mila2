105e18216309ee2005adfc212ef38559f9622f6a
Joël <57440945+jofmi@users.noreply.github.com>
2023-12-29 18:07:09 +0100

    feat: improve registration form

--- plugins/setup.ts
+++ plugins/setup.ts
@@ -7,21 +7,21 @@ export default defineNuxtPlugin({
 
     const is_legal = [
       {
-        key: "directus_users.memberships_person_type",
+        key: "directus_users__memberships_person_type",
         value: "legal",
       },
     ];
 
     const is_natural = [
       {
-        key: "directus_users.memberships_person_type",
+        key: "directus_users__memberships_person_type",
         value: "natural",
       },
     ];
 
     const is_sepa = [
       {
-        key: "directus_users.payments_type",
+        key: "directus_users__payments_type",
         value: "sepa",
       },
     ];
@@ -48,24 +48,22 @@ export default defineNuxtPlugin({
     ];
 
     form.value = {
-      title: "Membership application form",
+      title: "MILA Membership Application",
       public: true,
       submitMode: "postNuxt",
       submitPath: "/api/memberships/register",
-      fields: {
-        section_welcome: {
+      submitLabel: "Submit application",
+      fields: [
+        {
           type: "section",
           order: 10,
           title: "Willkommen bei MILA!",
           description: "t:mila_form_intro",
         },
-        section_person_type: {
-          type: "section",
-          order: 100,
-          title: "Type of person",
-        },
-        "directus_users.memberships_person_type": {
-          type: "select-radio",
+        {
+          type: "select",
+          key: "directus_users__memberships_person_type",
+          expand: true,
           label: "t:memberships_form_ptype",
           default: "natural",
           order: 110,
@@ -81,79 +79,87 @@ export default defineNuxtPlugin({
             },
           ],
         },
-        section_account: {
+        {
           type: "section",
           order: 200,
           title: "User account",
         },
-        "directus_users.email": {
+        {
+          key: "directus_users__email",
           label: "Email",
           type: "email",
           order: 210,
           required: true,
           icon: "i-mi-mail",
         },
-        "directus_users.password": {
+        {
           label: "Password",
+          key: "directus_users__password",
           type: "password",
           order: 220,
           required: true,
           icon: "i-mi-lock",
         },
-        section_organization: {
+        {
           type: "section",
           title: "Organization",
           order: 300,
           conditions: is_legal,
         },
-        "directus_users.memberships_organization_name": {
+        {
           label: "Organization name",
+          key: "directus_users__memberships_organization_name",
           type: "text",
           order: 310,
           required: true,
           conditions: is_legal,
         },
-        "directus_users.memberships_organization_type": {
+        {
           label: "Organization type",
+          key: "directus_users__memberships_organization_type",
           type: "text",
           order: 320,
           required: true,
           conditions: is_legal,
         },
-        "directus_users.memberships_organization_id": {
+        {
           label: "Organization ID",
+          key: "directus_users__memberships_organization_id",
           type: "text",
           order: 330,
           required: true,
           conditions: is_legal,
         },
-        section_personal_data: {
+        {
           type: "section",
           order: 400,
           title: "Personal data",
           conditions: is_natural,
         },
-        section_personal_data_legal: {
+        {
           type: "section",
           order: 401,
           title: "Organization Contact person",
           conditions: is_legal,
         },
-        "directus_users.first_name": {
+        {
           type: "text",
+          key: "directus_users__first_name",
           order: 410,
           required: true,
           label: "First name",
         },
-        "directus_users.last_name": {
+        {
           type: "text",
+          key: "directus_users__last_name",
           order: 420,
           required: true,
           label: "Last name",
         },
-        "directus_users.memberships_gender": {
+        {
           type: "select",
           order: 430,
+          key: "directus_users__memberships_gender",
           label: "Gender",
           required: true,
           choices: [
@@ -171,106 +177,114 @@ export default defineNuxtPlugin({
             },
           ],
         },
-        "directus_users.memberships_phone": {
+        {
           type: "text",
+          key: "directus_users__memberships_phone",
           order: 440,
           label: "Phone",
           icon: "i-mi-call",
         },
-        "directus_users.memberships_birthday": {
+        {
           label: "Birthday",
+          key: "directus_users__memberships_birthday",
           type: "date",
           order: 450,
-          default: "2000-01-01",
           required: true,
           conditions: is_natural,
         },
-        "directus_users.memberships_occupation": {
+        {
           label: "Occupation",
+          key: "directus_users__memberships_occupation",
           type: "text",
           order: 460,
           required: true,
           conditions: is_natural,
           icon: "i-system-uicons-briefcase",
         },
-        section_address: {
+        {
           type: "section",
           order: 500,
           title: "Address",
           conditions: is_natural,
         },
-        section_address_legal: {
+        {
           type: "section",
           order: 501,
           title: "Organization Address",
           conditions: is_legal,
         },
-        "directus_users.memberships_street": {
+        {
           label: "Street",
+          key: "directus_users__memberships_street",
           type: "text",
           order: 510,
           required: true,
         },
-        "directus_users.memberships_streetnumber": {
+        {
           label: "Number",
           type: "text",
+          key: "directus_users__memberships_streetnumber",
           order: 511,
           width: "xs",
           required: true,
         },
-        "directus_users.memberships_stair": {
+        {
           label: "Stair",
+          key: "directus_users__memberships_stair",
           type: "text",
           order: 512,
           width: "xs",
         },
-        "directus_users.memberships_door": {
+        {
           label: "Door",
+          key: "directus_users__memberships_door",
           type: "text",
           order: 513,
           width: "xs",
         },
-        "directus_users.memberships_postcode": {
+        {
           label: "Postcode",
+          key: "directus_users__memberships_postcode",
           type: "text",
           order: 514,
           width: "xs",
           required: true,
         },
-        "directus_users.memberships_city": {
+        {
           label: "City",
+          key: "directus_users__memberships_city",
           type: "text",
           order: 515,
           required: true,
         },
-        "directus_users.memberships_country": {
+        {
           label: "Country",
+          key: "directus_users__memberships_country",
           type: "text",
           order: 516,
           required: true,
         },
-        section_membership: {
+        {
           type: "section",
           order: 600,
           title: "Type of membership",
-        },
-        description_mtype_orga: {
-          type: "description",
-          order: 601,
           description: "t:mila_form_mtype_orga",
           conditions: is_legal,
         },
-        description_mtype_natural: {
-          type: "description",
-          order: 601,
+        {
+          type: "section",
+          order: 600,
+          title: "Type of membership",
+
           description: "t:mila_form_mtype_natural",
           conditions: is_natural,
         },
-        "memberships.memberships_type": {
-          type: "select-radio",
+        {
+          type: "select",
+          key: "memberships__memberships_type",
+          expand: true,
           label: "t:memberships_form_mtype",
           required: true,
-          width: "full",
           order: 610,
           choices: [
             {
@@ -284,8 +298,10 @@ export default defineNuxtPlugin({
             },
           ],
         },
-        shares_options: {
-          type: "select-radio",
+        {
+          type: "select",
+          key: "memberships__memberships_shares_options",
+          expand: true,
           label: "How many shares do you want?",
           description: "t:mila_form_shares",
           required: true,
@@ -296,8 +312,9 @@ export default defineNuxtPlugin({
             { label: "Mehr Anteile", value: "more" },
           ],
         },
-        "memberships.memberships_shares": {
+        {
           type: "number",
+          key: "memberships__memberships_shares",
           label: "Number of shares (9 or more)",
           required: true,
           order: 620,
@@ -314,21 +331,21 @@ export default defineNuxtPlugin({
             },
           ],
         },
-        description_shares_normal: {
+        {
           type: "description",
           order: 630,
           label: "Chosen shares",
           description: "t:mila_form_shares_normal",
           conditions: shares_normal,
         },
-        description_shares_social: {
+        {
           type: "description",
           order: 630,
           label: "Chosen shares",
           description: "t:mila_form_shares_social",
           conditions: shares_social,
         },
-        description_shares_more: {
+        {
           type: "description",
           order: 630,
           label: "Chosen shares",
@@ -336,14 +353,15 @@ export default defineNuxtPlugin({
           conditions: shares_more,
         },
         // TODO: CALC TOTAL VALUE
-        section_payment: {
+        {
           type: "section",
           order: 700,
           title: "Payment details",
           description: "t:mila_form_payment",
         },
-        "directus_users.payments_type": {
+        {
           label: "Payment type",
+          key: "directus_users__payments_type",
           type: "select",
           order: 710,
           // width: "full",
@@ -359,44 +377,52 @@ export default defineNuxtPlugin({
             },
           ],
         },
-        "directus_users.payments_account_iban": {
+        {
           label: "Bank account IBAN",
+          key: "directus_users__payments_account_iban",
           type: "text",
           order: 720,
           conditions: is_sepa,
           required: true,
         },
-        "directus_users.payments_account_owner": {
+        {
           label: "Bank account owner",
+          key: "directus_users__payments_account_owner",
           type: "text",
           order: 730,
           conditions: is_sepa,
           required: true,
         },
-        section_survey: {
+        {
           type: "section",
           order: 800,
           title: "Survey",
           description: "t:mila_form_survey",
         },
-        "directus_users.mila_survey_contact": {
+        {
           label: "How did you hear about us?",
+          key: "directus_users__mila_survey_contact",
           type: "textarea",
           order: 810,
         },
-        "directus_users.mila_survey_motive": {
+        {
           label: "What convinced you to join MILA?",
+          key: "directus_users__mila_survey_motive",
           type: "textarea",
           order: 820,
         },
-        // TODO: Select multiple
-        "directus_users.mila_groups_interested": {
+        {
+          type: "clear",
+          order: 825,
+        },
+        {
           label: "Would you be interested to join a working group?",
+          key: "directus_users__mila_groups_interested",
           description:
             "You can find more information about the working groups here: https://www.mila.wien/de/mitmachen/arbeitsgruppen/",
           type: "select",
+          multiple: true,
           order: 830,
-          required: true,
           choices: [
             {
               value: "1",
@@ -408,11 +434,12 @@ export default defineNuxtPlugin({
             },
           ],
         },
-        "directus_users.mila_skills": {
+        {
           label: "What are your occupations/skills/interests?",
+          key: "directus_users__mila_skills",
           type: "select",
+          multiple: true,
           order: 840,
-          required: true,
           choices: [
             {
               value: "1",
@@ -424,53 +451,56 @@ export default defineNuxtPlugin({
             },
           ],
         },
-        section_final: {
+        {
           type: "section",
-          title: "Terms",
+          title: "Conditions",
           order: 900,
         },
-        terms2: {
-          type: "toggle",
+        {
+          type: "checkbox",
+          key: "_statutes_approval",
           label: "Statutes",
           description: "t:mila_form_check2",
           order: 920,
           width: "full",
           required: true,
         },
-        terms3: {
-          type: "toggle",
+        {
+          type: "checkbox",
+          key: "_data_approval",
           label: "Data use",
           description: "t:mila_form_check3",
           order: 930,
           width: "full",
           required: true,
         },
-        "directus_users.mila_pr_approved": {
-          type: "toggle",
+        {
+          type: "checkbox",
+          key: "directus_users__mila_pr_approved",
           label: "PR Work",
           description: "t:mila_form_check1",
           order: 931,
           width: "full",
         },
-        final1: {
+        {
           type: "description",
           order: 940,
           label: "Liability",
           description: "t:mila_form_final1",
         },
-        final2: {
+        {
           type: "description",
           order: 950,
           label: "Payout upon termination",
           description: "t:mila_form_final2",
         },
-        final3: {
+        {
           type: "description",
           order: 960,
           label: "Revocation",
           description: "t:mila_form_final3",
         },
-      },
+      ],
     };
   },
 });
